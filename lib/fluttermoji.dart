fd8f0d57671930ae2e0f95d5f4a54c8a0debed57
Maxime Guilbert <mx.guilbert@gmail.com>
2024-06-21 15:30:04 -0400

    fix: TMP

--- lib/fluttermoji.dart
+++ lib/fluttermoji.dart
@@ -1,6 +1,7 @@
 library avatar_maker;
 
 export './fluttermojiCustomizer.dart';
+
 export './fluttermojiCircleAvatar.dart';
 export './fluttermojiController.dart';
 export './fluttermojiFunctions.dart';
