fbc80e6f0f381a395a3bc34418fc96bd6923d873
Maxime Guilbert <mx.guilbert@gmail.com>
2024-06-24 18:36:58 -0400

    chore: Unit tests AvatarMakerSaveWidget

--- test/helpers.dart
+++ test/helpers.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 
-
 extension WidgetTesterExtension on WidgetTester {
   Future<void> pumpMaterialApp(Widget widget) {
     return pumpWidget(
@@ -14,6 +13,4 @@ extension WidgetTesterExtension on WidgetTester {
       ),
     );
   }
-
 }
-
